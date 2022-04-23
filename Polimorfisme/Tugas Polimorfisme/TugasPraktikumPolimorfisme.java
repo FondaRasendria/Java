239b956128d1871c65da2e41db81ece873b74cd1
FondaRasendria <97221240+FondaRasendria@users.noreply.github.com>
2022-04-23 20:30:23 +0700

    Add files via upload

---
+++ Polimorfisme/Tugas Polimorfisme/TugasPraktikumPolimorfisme.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tugaspraktikumpolimorfisme;
+
+/**
+ *
+ * @author Fonda
+ */
+public class TugasPraktikumPolimorfisme {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Rect a = new Rect(2,4,5,10);
+        System.out.println("Union " + a.IsInside(1, 3));
+        
+    }
+    
+}
